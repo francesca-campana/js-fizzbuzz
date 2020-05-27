a53a535b451d24f763c5ccc442e09ef8e33310da
francesca-campana <franx3rox@gmail.com>
2020-05-27 17:59:59 +0200

    indentazione

--- js/script.js
+++ js/script.js
@@ -3,23 +3,26 @@
 // Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
 var elementoLista = document.getElementById('lista');
+
 for (var i = 1; i <= 100; i++) {
+
   var intruso = i;
+
   if (i % 15 === 0 ) {
-  // console.log('FizzBuzz');
-  intruso = 'FizzBuzz';
+    // console.log('FizzBuzz');
+    intruso = 'FizzBuzz';
 
-    } else if (i % 3 === 0 ) {
-      // console.log('Fizz');
-      intruso = 'Fizz';
-    } else if (i % 5 === 0 ) {
-      // console.log('Buzz');
-      intruso = 'Buzz';
+  } else if (i % 3 === 0 ) {
+    // console.log('Fizz');
+    intruso = 'Fizz';
 
-    } else {
-      // console.log(i);
+  } else if (i % 5 === 0 ) {
+    // console.log('Buzz');
+    intruso = 'Buzz';
 
-    }
-    elementoLista.innerHTML += '<li>' + intruso  + '</li>';
-
+  } else {
+    // console.log(i);
   }
+
+  elementoLista.innerHTML += '<li>' + intruso  + '</li>';
+}
